80bdb4ab4f5c066388cd26de1850ff95209cc6dd
beverts312 <texas312@gmail.com>
2016-01-05 20:41:05 +0000

    Updated setcreds.sh

--- src/setcreds.sh
+++ src/setcreds.sh
@@ -1,3 +1,3 @@
 echo 'aws_access_key:${AWS_ACCESS}' >> ${WORKDIR}/group_vars/all/credentials.yml
-echo 'aws_secret_key:${AWS_ACCESS}' >> ${WORKDIR}/group_vars/all/credentials.yml
-echo 'aws_keypair:${AWS_ACCESS}' >> ${WORKDIR}/group_vars/all/credentials.yml
+echo 'aws_secret_key:${AWS_SECRET}' >> ${WORKDIR}/group_vars/all/credentials.yml
+echo 'aws_keypair:${AWS_KEYPAIR}' >> ${WORKDIR}/group_vars/all/credentials.yml
