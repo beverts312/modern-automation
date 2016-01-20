263c79c4cbea8b0c66a90f2f6af245c28150a78b
beverts312 <texas312@gmail.com>
2016-01-19 21:02:30 -0600

    removed debug stuff

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}/group_vars/all/common.yml.template > ${WORKDIR}/group_vars/all/common.yml
 rm ${WORKDIR}/group_vars/all/common.yml.template
-cat ${WORKDIR}/group_vars/all/common.yml
-#ansible-playbook $PLAYBOOK 
+ansible-playbook $PLAYBOOK 
