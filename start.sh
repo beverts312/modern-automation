007a35ab94243318c8a35bbd98d56b21b6778a62
beverts312 <texas312@gmail.com>
2016-01-19 20:46:10 -0600

    clean up,trying to make perl work

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}\group_vars\all\common.yml.template > ${WORKDIR}\group_vars\all\common.yml
-perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}\group_vars\all\credentials.yml.template > ${WORKDIR}\group_vars\all\credentials.yml
-rm ${WORKDIR}\group_vars\all\common.yml.template
-rm ${WORKDIR}\group_vars\all\credentials.yml.template
+perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}\group_vars\all\common.yml > ${WORKDIR}\group_vars\all\common.yml
 cat ${WORKDIR}\group_vars\all\common.yml
-cat ${WORKDIR}\group_vars\all\credentials.yml
 #ansible-playbook $PLAYBOOK 
