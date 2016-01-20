16d828141e250235f044d13e9ce2989304f8ffb8
beverts312 <texas312@gmail.com>
2016-01-20 02:42:30 +0000

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}\group_vars\all\common.yml > ${WORKDIR}\group_vars\all\common.yml
-perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}\group_vars\all\credentials.yml > ${WORKDIR}\group_vars\all\credentials.yml
+perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}\group_vars\all\common.yml.template > ${WORKDIR}\group_vars\all\common.yml
+perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}\group_vars\all\credentials.yml.template > ${WORKDIR}\group_vars\all\credentials.yml
+rm ${WORKDIR}\group_vars\all\common.yml.template
+rm ${WORKDIR}\group_vars\all\credentials.yml.template
 cat ${WORKDIR}\group_vars\all\common.yml
 cat ${WORKDIR}\group_vars\all\credentials.yml
 #ansible-playbook $PLAYBOOK 
