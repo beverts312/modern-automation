637aa1ed72d37d62915cf9de7cea75e2e05ebab5
beverts312 <texas312@gmail.com>
2016-01-20 02:36:42 +0000

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}\group_vars\all\common.yml > ${WORKDIR}\group_vars\all\common.yml
 perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}\group_vars\all\credentials.yml > ${WORKDIR}\group_vars\all\credentials.yml
-ansible-playbook $PLAYBOOK 
+cat ${WORKDIR}\group_vars\all\common.yml
+cat ${WORKDIR}\group_vars\all\credentials.yml
+#ansible-playbook $PLAYBOOK 
