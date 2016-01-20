477692071a39557b930ba30e5d1f78eb82bb46d4
beverts312 <texas312@gmail.com>
2016-01-19 20:58:12 -0600

    changed back to template

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}/group_vars/all/common.yml > ${WORKDIR}/group_vars/all/common.yml
+perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}/group_vars/all/common.yml.template > ${WORKDIR}/group_vars/all/common.yml
+rm ${WORKDIR}/group_vars/all/common.yml.template
 cat ${WORKDIR}/group_vars/all/common.yml
 #ansible-playbook $PLAYBOOK 
