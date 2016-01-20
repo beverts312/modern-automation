3a95c739e78eee8525dfa03292e2e21bf2cc8537
beverts312 <texas312@gmail.com>
2016-01-20 01:38:36 +0000

    Updated setcreds.sh

--- playbooks/setcreds.sh
+++ playbooks/setcreds.sh
@@ -1,5 +1,4 @@
 echo 'aws_access_key:${AWS_ACCESS}' >> ${WORKDIR}/group_vars/all/credentials.yml
 echo 'aws_secret_key:${AWS_SECRET}' >> ${WORKDIR}/group_vars/all/credentials.yml
 echo 'aws_keypair:${AWS_KEYPAIR}' >> ${WORKDIR}/group_vars/all/credentials.yml
-echo ${PLAYBOOK} 
-echo ${EXTRA_VARS}
+cat ${WORKDIR}/group_vars/all/credentials.yml
