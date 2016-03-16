1a73599dc36bc124a2a059be9cce80b44f244c73
beverts312 <texas312@gmail.com>
2016-03-16 07:21:40 -0500

    removed docker stuff

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-perl -p -e 's/##([^#]+)##/defined $ENV{$1} ? $ENV{$1} : die "$1 is not set in $ARGV"/eg;' ${WORKDIR}/group_vars/all/common.yml.template > ${WORKDIR}/group_vars/all/common.yml
-rm ${WORKDIR}/group_vars/all/common.yml.template
-ansible-playbook $PLAYBOOK 
